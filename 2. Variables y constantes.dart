39cd91b49c02670fc27e2774433b57676276bc6c
AndresED <andres30xed@gmail.com>
2019-01-06 21:59:42 -0500

    Variables y constantes

---
+++ 2. Variables y constantes.dart
@@ -0,0 +1,9 @@
+void main(){
+  //var edad = 26; //Variable 
+  int edad = 26; //Variable => tambien puedo especificar el tipo de dato de una variable
+  //var nombre = "Andres Esquivel";
+  String nombre = "Andres Esquivel"; //Variable => tambien puedo especificar el tipo de dato de una variable
+  final double pi = 3.14; // final me indica que la variable pi es una constante
+  print("Mi llamo ${nombre} y mi edad es ${edad}");
+  print("El valor de pi es ${pi}");
+}
