e8362afca615f1fa2a672f75d7dbaf483caf7f06
AndresED <andres30xed@gmail.com>
2019-01-06 22:26:35 -0500

    Tipo de datos

---
+++ 3.Tipos de datos.dart
@@ -0,0 +1,21 @@
+void main(){
+  /* 
+   	En dart hay 5 tipo de datos 
+  	  - Los Numbers
+      - String
+      - Boolean
+      - Lists(Arrays)
+      - Maps(Dictionary)
+    En dart los tipos de datos son objetos lo que implica que tienen un valor por defecto 		o un zero-value y este valor es null
+ 	*/
+  
+  var number = 15;
+  var double1 = 15.83;
+  print(number);
+  print(double1);
+  
+ String nombre = "Andrés Esquivel";
+  print("Bienvenido a Dart ${nombre}");
+  var isLogin =  true;
+  print("¿He iniciado sesión? ${isLogin}");
+}
